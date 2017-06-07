e2a2faa3e88260db612c2a49f8371bea1878b542
Lyman Epp <lyman@epptech.com>
2017-06-07 11:13:13 -0500

    Made spaces and tabs consistent

--- smartapps/lymanepp/virtual-presence.src/virtual-presence.groovy
+++ smartapps/lymanepp/virtual-presence.src/virtual-presence.groovy
@@ -15,55 +15,55 @@
  *  Author: Lyman Epp, Based on work by fuzzysb with thanks
  */
 definition(
-    name: "Virtual Presence",
-    namespace: "lymanepp",
-    author: "Lyman Epp",
-    description: "Triggers Virtual Presence when HTTP GET Request is received",
-    category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+	name: "Virtual Presence",
+	namespace: "lymanepp",
+	author: "Lyman Epp",
+	description: "Triggers Virtual Presence when HTTP GET Request is received",
+	category: "Convenience",
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 preferences {
-    section(title: "Select Devices") {
-        input "virtualPresence", "capability.presenceSensor", title: "Select Virtual Presence Sensors", required: true, multiple: true
-    }
+	section(title: "Select Devices") {
+		input "virtualPresence", "capability.presenceSensor", title: "Select Virtual Presence Sensors", required: true, multiple: true
+	}
 }
 
 def installed() {
 	createAccessToken()
 	getToken()
 	log.debug "Installed Phone with rest api: $app.id"
-    log.debug "Installed Phone with token: $state.accessToken"
+	log.debug "Installed Phone with token: $state.accessToken"
 }
 
 def updated() {
 	log.debug "Updated Phone with rest api: $app.id"
-    log.debug "Updated Phone with token: $state.accessToken"
+	log.debug "Updated Phone with token: $state.accessToken"
 }
 
 mappings {
   path("/presence/:sensor/:status") {
-    action: [
-      GET: "updatePresence"
-    ]
+	action: [
+	  GET: "updatePresence"
+	]
   }
 }
 
 def updatePresence() {
 	log.debug "Presence $params.sensor $params.status"
-    virtualPresence.each {
+	virtualPresence.each {
 		if (it.name.equalsIgnoreCase(params.sensor)) {
-            if (params.status.equalsIgnoreCase("home")) {
-            	log.debug "Executing arrived for $it.name"
-                it.arrived();
-            } else {
-            	log.debug "Executing departed for $it.name"
-                it.departed();
-            }
-        }
-    }
+			if (params.status.equalsIgnoreCase("home")) {
+				log.debug "Executing arrived for $it.name"
+				it.arrived();
+			} else {
+				log.debug "Executing departed for $it.name"
+				it.departed();
+			}
+		}
+	}
 }
 
 def getToken() {
@@ -73,7 +73,7 @@ def getToken() {
 			log.debug "Creating new Access Token: $state.accessToken"
 		} catch (ex) {
 			log.debug "Did you enable OAuth in SmartApp IDE settings?"
-            log.debug ex
+			log.debug ex
 		}
 	}
 }
